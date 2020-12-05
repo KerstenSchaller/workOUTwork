9f629c42bdac1fa8436a72c71c408da3d6b08022
Kersten Schaller <kersten.schaller@outlook.com>
2020-12-05 16:33:09 +0100

    implemented event handler for mouse and key without application in focus

---
+++ wow/wow/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Gma.System.MouseKeyHook;
+using Gma.System.MouseKeyHook.Implementation;
+
+
+namespace wow
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            MouseKeyHandler mouseKeyHandler = new MouseKeyHandler();
+            mouseKeyHandler.Subscribe(Hook.GlobalEvents());
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+
+
+        }
+
+    }
+}
---
+++ wow/wow/mouseKeyHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Gma.System.MouseKeyHook;
+using Gma.System.MouseKeyHook.Implementation;
+
+using System.Windows.Forms;
+
+namespace wow
+{
+    class MouseKeyHandler
+    {
+
+        private IKeyboardMouseEvents m_Events;
+
+        public void Subscribe(IKeyboardMouseEvents events)
+        {
+            m_Events = events;
+            m_Events.KeyPress += HookManager_KeyPress;
+            m_Events.MouseMove += HookManager_MouseMove;
+
+
+
+        }
+        /*Mouse Handlers*/
+        private void HookManager_MouseMove(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("mouse moved");
+
+        }
+
+        private void HookManager_KeyPress(object sender, KeyPressEventArgs e)
+        {
+             Console.WriteLine("Key pressed");
+        }
+
+
+        private void Unsubscribe()
+        {
+            if (m_Events == null) return;
+            m_Events.KeyPress -= HookManager_KeyPress;
+            m_Events.MouseMove -= HookManager_MouseMove;
+            m_Events.Dispose();
+            m_Events = null;
+        }
+    }
+}
