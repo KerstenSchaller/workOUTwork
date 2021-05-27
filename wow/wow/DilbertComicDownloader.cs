fb679513505eb6971222cf151c076a8e727d83b5
Kersten Schaller <kersten.schaller@outlook.com>
2021-05-27 18:18:57 +0200

    added Dilbert Downloader and included the image on the lockscreen, scaled

---
+++ wow/wow/DilbertComicDownloader.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wow
+{
+    class DilbertComicDownloader : WebClient
+    {
+        string httpContent = "";
+        string ImageUrl = "";
+        List<string> all_tags = new List<string>();
+
+        public bool AcceptAllCertifications(object sender, System.Security.Cryptography.X509Certificates.X509Certificate certification, System.Security.Cryptography.X509Certificates.X509Chain chain, System.Net.Security.SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+
+        public Image getDilbertComicImageByDate(DateTime date)
+        {
+            Image image;
+            string url = getDilbertComícUrlByDate(date);
+            image = GetImageFromURL(url);
+            return image;
+
+        }
+
+
+        private string getDilbertComícUrlByDate(DateTime date)
+        {
+            string adress = "https://dilbert.com/strip/";
+            adress += date.Year;
+            adress += "-";
+            adress += (date.Month < 10) ? "0" : "";// prepend 0 if needed
+            adress += date.Month;
+            adress += "-";
+            adress += (date.Day < 10) ? "0" : "";// prepend 0 if needed
+            adress += date.Day;
+            this.getHtml(adress);
+            return this.getImageUrl();
+        }
+
+        public string getHtml(string address)
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            //Use SecurityProtocolType.Ssl3 if needed for compatibility reasons
+
+            //Use callback for https connection certificate acception
+            ServicePointManager.ServerCertificateValidationCallback +=
+                delegate (object sender, X509Certificate cert, X509Chain chain, SslPolicyErrors sslError)
+                {
+                    bool validationResult = true;
+                    return validationResult;
+                };
+
+
+            Uri uri = new Uri(address);
+            WebRequest request = GetWebRequest(uri);
+            WebResponse response = request.GetResponse();
+            StreamReader sr = new StreamReader(response.GetResponseStream(), System.Text.Encoding.UTF8);
+            string result = sr.ReadToEnd();
+            httpContent = result;
+            return result;
+        }
+
+
+
+        private Image GetImageFromURL(string url)
+        {
+            HttpWebRequest httpWebRequest = (HttpWebRequest)HttpWebRequest.Create(url);
+            HttpWebResponse httpWebReponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            Stream stream = httpWebReponse.GetResponseStream();
+            return Image.FromStream(stream);
+        }
+
+        public string getImageUrl()
+        {
+            string UrlStartstring = "https://assets.amuniversal";
+
+            int startIndex = httpContent.IndexOf(UrlStartstring);
+            int endindex = httpContent.IndexOf("\"/>\n", startIndex);
+
+            int startindexUrl = startIndex;
+            int endindexUrl = endindex;
+            int length = endindexUrl - startIndex;
+            string imageUrl = httpContent.Substring(startindexUrl, length);
+            return imageUrl;
+        }
+
+
+
+
+
+        protected override WebRequest GetWebRequest(Uri uri)
+        {
+            HttpWebRequest request = base.GetWebRequest(uri) as HttpWebRequest;
+            request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+            return request;
+        }
+
+
+    }
+}
--- wow/wow/ScreenImageComposer.cs
+++ wow/wow/ScreenImageComposer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -60,11 +62,62 @@ namespace wow
             textBrush.Dispose();
             drawing.Dispose();
 
+            Image dilbertImage = addDilbertComic(img);
+
             if (filename != "") 
             { 
-                new Bitmap(img).Save(filename);
+                new Bitmap(dilbertImage).Save(filename);
             }
             return img;
         }
-}
+
+        public Image addDilbertComic(Image backgroundImage) 
+        {
+            DilbertComicDownloader dilbertComicDownloader = new DilbertComicDownloader();
+            var dilbertComic = dilbertComicDownloader.getDilbertComicImageByDate(DateTime.Now.Date);
+
+            int w = dilbertComic.Width;
+            int h = dilbertComic.Height;
+            float m = 500f/w;
+            int newHeight = (int)(h * m);
+
+            Image embeddedImage = embeddImage(backgroundImage, dilbertComic, 50,50,500, newHeight);
+            return embeddedImage;
+        }
+
+        public Image embeddImage(Image backImg, Image frontImage,int xpos, int ypos, int width, int height) 
+        {
+
+            Graphics g = Graphics.FromImage(backImg);
+            var resizedfrontImage = ResizeImage(frontImage, width, height);
+            g.DrawImage(frontImage, xpos, ypos);
+            return backImg;
+        }
+
+        private static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            // function taken from: https://stackoverflow.com/questions/1922040/how-to-resize-an-image-c-sharp
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+    }
 }
